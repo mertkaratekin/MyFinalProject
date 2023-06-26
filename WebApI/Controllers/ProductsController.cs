a99a38f8ef065f4fbe760c3f6f678cd72f693913
Mert Karatekin <80035118+mertkaratekin@users.noreply.github.com>
2023-06-27 00:44:36 +0300

    27.06.2023

---
+++ WebApI/Controllers/ProductsController.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]  /*ATTRIBUTE = imzalama yöntemi*/
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //IoC Container -- değişimin konrolü
+        IProductService _productService;//gevşek bağımlılık
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _productService.GetAll();
+            if (result.Successs)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Successs)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Successs)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+       
+    }
+}
